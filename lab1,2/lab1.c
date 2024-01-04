04247739714bc443f6306d96e8dee474bcda7f68
velo112 <155531156+velo112@users.noreply.github.com>
2024-01-04 14:10:31 +0900

    Add files via upload

---
+++ lab1,2/lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("==================================\n");
+	printf("DDDD   EEEEE  L      L       AAA \n");
+	printf("D   D  E      L      L      A   A\n");
+	printf("D   D  EEEEE  L      L      AAAAA\n");
+	printf("D   D  E      L      L      A   A \n");
+	printf("DDDD   EEEEE  LLLLL  LLLLL  A   A \n");
+	printf("==================================\n");
+}
---
+++ lab1,2/lab2.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	printf("Input two integers: ");
+	scanf("%d %d", &a, &b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d %% %d = %d\n", a, b, a % b);
+}
